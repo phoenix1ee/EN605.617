5d8595ec1e4b77fc24cbc3ef251d0acf4daa645b
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2026-02-16 17:36:48 -0800

    restructure and update tests
    Update function of tests in globla_memory.cu
    add host function test wrapper for adding different host test.
    add GPU function test wrapper for adding different GPU test.
    update the Interleaved and NonInterleaved CPU test and add a wrapper
    add the Interleaved and NonInterleaved GPU test inside a wrapper function

--- module4/host_memory.cu
+++ module4/host_memory.cu
@@ -11,7 +11,8 @@ void saxpy(int n, float a, float *x, float *y)
 
 int main(void)
 {
-  int N = 1<<20;
+  for(int i=10;i<21;i=i+2){
+  int N = 1<<i;
   float *x, *y, *d_x, *d_y;
   x = (float*)malloc(N*sizeof(float));
   y = (float*)malloc(N*sizeof(float));
@@ -20,22 +21,22 @@ int main(void)
   cudaMalloc(&d_y, N*sizeof(float));
 
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+    x[i] = 5.1245864f;
+    y[i] = 8.1573f;
   }
 
   cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
 
   // Perform SAXPY on 1M elements
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+  saxpy<<<(N+255)/256, 256>>>(N, 2.7891f, d_x, d_y);
 
   cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
 
   float maxError = 0.0f;
   for (int i = 0; i < N; i++){
-    maxError = max(maxError, abs(y[i]-4.0f));
-    printf("y[%d]=%f\n",i,y[i]);
+    maxError = max(maxError, abs(y[i]-22.45028392824f));
+    //printf("y[%d]=%f\n",i,y[i]);
   }
   printf("Max error: %f\n", maxError);
 
@@ -43,4 +44,5 @@ int main(void)
   cudaFree(d_y);
   free(x);
   free(y);
+}
 }
